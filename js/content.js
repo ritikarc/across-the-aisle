20c4ef9cf00781b09e5e65361caa72da4f4fce11
Alexa Ramachandran <t-alrama@microsoft.com>
2019-07-22 15:58:16 -0700

    Added CNN to list of sites

--- js/content.js
+++ js/content.js
@@ -1,8 +1,9 @@
 // get the URL of the page
 var url = document.location.href;
 
-// if not on a docs.microsoft.com domain
-if (url.indexOf("//docs.microsoft.com") <= -1) {
+ //if not on a docs.microsoft.com domain
+if ((url.indexOf("//docs.microsoft.com") <= -1) &&
+    (url.indexOf("//www.cnn.com") <= -1)) {
     // send inactive icons
     browser.runtime.sendMessage({
         "iconPath20": "images/inactive20.png",
