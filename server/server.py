0e6a8ddf46fcfe209ba39919af9ccd4e0ec7b121
Garand Tyson <gttyson@stanford.edu>
2019-07-23 16:36:17 -0700

    Added python server

---
+++ server/server.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+ 
+from http.server import BaseHTTPRequestHandler, HTTPServer
+ 
+# HTTPRequestHandler class
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+ 
+  # GET
+  def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+ 
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+ 
+        # Send message back to client
+        message = "Hello world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+ 
+def do_POST(self):
+        self._set_headers()
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD': 'POST'}
+        )
+        print form.getvalue("foo")
+        print form.getvalue("bin")
+        self.wfile.write("<html><body><h1>POST Request Received!</h1></body></html>")
+        return
+
+def run():
+  print('starting server...')
+ 
+  # Server settings
+  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+  server_address = ('127.0.0.1', 8081)
+  httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+  print('running server on port 8081')
+  httpd.serve_forever()
+ 
+ 
+run()
