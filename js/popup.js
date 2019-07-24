6754364f51953a861597b5633b7abc2be6087368
Alexa Ramachandran <t-alrama@microsoft.com>
2019-07-24 08:17:02 -0700

    Better logos

--- js/popup.js
+++ js/popup.js
@@ -1,42 +1,15 @@
 // get the buttons by id
-// let aliceblue = document.getElementById('aliceblue');
-// let cornsilk = document.getElementById('cornsilk');
-// let reset = document.getElementById('reset');
 //let CNNsourcerating = document.getElementById('sourcebiasrating');
-alert("Test");
+//alert("Test");
 
 let sourceratingbias = document.getElementById('sourceratingbias');
-//var sourcebias = "";
-//var sourcerating = "Hmm"
+
 window.addEventListener('load', (event) => {
   sourcebias();
 });
 
 function sourcebias() {
-  sourceratingbias.innerText = "CNN";
-  //if(url.indexOf())
+    //if (!(url.indexOf("//www.cnn.com") <= -1)) {
+        sourceratingbias.innerText = "CNN";
+    //}
 }
-
-// use tabs.insertCSS to change header color on button click
-
-// // aliceblue
-// aliceblue.onclick = () => {
-//     //browser.pageAction.setPopup("hello");
-//     Console.log("Hello world");
-//   //browser.tabs.insertCSS({code: "body { background: aliceblue !important; }"});
-// };
-
-// if(url.indexOf("//cnn.com") > -1 ) {
-//   sourcebias = "CNN";
-// } 
-
-// // cornsilk
-// cornsilk.onclick = () => {
-//   browser.tabs.insertCSS({code: "body { background: cornsilk !important; }"});
-// };
-
-// // back to original
-// reset.onclick = () => {
-//   browser.tabs.insertCSS({code: "body { background: none !important; }"});
-// };
-sourcebias();
